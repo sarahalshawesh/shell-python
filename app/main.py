170dcb434052adfe283ad45e43a79280c19cf819
Sarah Robinson <sarahrose@Sarahs-MacBook-Pro-8.local>
2025-11-22 21:33:57 +0000

    fixes bug for exit

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ def main():
         sys.stdout.flush()
         user_input = sys.stdin.readline().strip()
         sys.stdout.write(f"{user_input}: command not found\n")
-        if user_input.contains("exit"):
+        if "exit" in user_input:
             sys.exit()
 
 
