4bc2613e0e484e9552fd39558894b877711639cf
Sarah Robinson <sarahrose@Sarahs-MacBook-Pro-8.local>
2025-11-23 16:16:16 +0000

    refactoring to commands list and adding logic for type

--- app/main.py
+++ app/main.py
@@ -1,32 +1,31 @@
 import sys
 
+commands = {
+    "exit" : exit(0),
+    "echo": echo(input),
+    "type": type(input)
+}
+
+def echo(input):
+    print(input[5:])
+
+def type(input, commands):
+    for command in commands:
+        if command in commands:
+            print(f"{command}is a shell builtin")
+        else: 
+            print(f"{command}: command not found\n")
 
 def main():
 
-    def exit_command(input):
-        if input.startswith("type exit"):
-            print("exit is a shell builtin")
-        else:
-            exit(0)
-        return
-
-    def echo(input):
-        if input.startswith("type echo"):
-            print("echo is a shell builtin")
-        else:
-            print(user_input[5:])
-        return
-    
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
         user_input = sys.stdin.readline().strip()
-        
+        command = user_input.split()[0]
 
-        if "exit" in user_input:
-            exit_command(user_input)
-        elif "echo" in user_input:
-            echo(user_input) 
+        if command in commands:
+            commands[command]()
         else:
             sys.stdout.write(f"{user_input}: command not found\n")
 
