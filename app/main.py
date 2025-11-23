8ce5c3deee734486af07608358104b65e1790a3b
Sarah Robinson <sarahrose@Sarahs-MacBook-Pro-8.local>
2025-11-23 03:10:14 +0000

    refactoring

--- app/main.py
+++ app/main.py
@@ -1,19 +1,25 @@
 import sys
 
-commands = {
-    "exit" : exit(0),
-    "echo" : print(user_input[5:]),
-    "type echo": print("echo is a shell builtin")
-    }
 
 def main():
+
+    def echo(input):
+        if input.startswith("type echo"):
+            print("echo is a shell builtin")
+        else:
+            print(user_input[5:])
+        return
+    
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
         user_input = sys.stdin.readline().strip()
-        command = user_input[0]
-        if command in commands:
-            commands.command
+        
+
+        if "exit" in user_input:
+            exit(0)
+        elif "echo" in user_input:
+            echo(user_input) 
         else:
             sys.stdout.write(f"{user_input}: command not found\n")
 
