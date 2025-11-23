79d88147cb53313d1e484edb55e4f1278e09606f
Sarah Robinson <sarahrose@Sarahs-MacBook-Pro-8.local>
2025-11-23 02:43:53 +0000

    debugging echo

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def main():
         if "exit" in user_input:
             exit(0)
         elif "echo" in user_input:
-            print(user_input[6:])
+            print(user_input[5:])
         else:
             sys.stdout.write(f"{user_input}: command not found\n")
 
