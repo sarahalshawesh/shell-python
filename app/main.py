52589c0d92b196e7cd457faa06a4e1a2b35ffce0
Sarah Robinson <sarahrose@Sarahs-MacBook-Pro-8.local>
2025-11-24 01:22:06 +0000

    adding path logic

--- app/main.py
+++ app/main.py
@@ -1,20 +1,20 @@
 import sys, os
 
-def exit_command():
+def exit_command(args):
     exit(0)
 
-def echo_command(input):
-    print(input[5:])
+def echo_command(args):
+    print(args[5:])
 
-def type_command(input, commands):
-    command = input.split()[1]
+def type_command(args):
+    command = args.split()[0]
     if command in commands:
         print(f"{command} is a shell builtin")
     elif command not in commands:
-        path = os.environ.get("PATH")
-        if path:
-            split_path = path.split(":")
-            print(split_path)
+        paths = os.environ.get("PATH").split(":")
+        for i in paths:
+            if os.path.isfile(f"{i}/{command}"):
+                print(f"{command} is {i}")
             
     else: 
         print(f"{command}: not found")
@@ -31,9 +31,9 @@ def main():
         sys.stdout.write("$ ")
         sys.stdout.flush()
         user_input = sys.stdin.readline().strip()
-        command, *args = user_input.split()
+        command, args = user_input.split()
         if command in commands:
-            commands[command](*args)
+            commands[command](args)
         else:
             sys.stdout.write(f"{user_input}: command not found\n")
 
