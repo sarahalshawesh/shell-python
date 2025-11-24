24a5bca66aa7dbc1727824582615b83c4e778003
Sarah Robinson <sarahrose@Sarahs-MacBook-Pro-8.local>
2025-11-24 02:47:54 +0000

    debugging exe

--- app/main.py
+++ app/main.py
@@ -32,21 +32,22 @@ def main():
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
-        user_input = sys.stdin.readline().strip()
-        command = user_input.split()[0]
-        args = user_input.split()[1:]
+        user_input = sys.stdin.readline().strip().split()
+
+        command = user_input[0]
+        args = user_input[1:]
 
         if command in commands:
             commands[command](args)
 
-        elif command not in commands:
+        else:
             paths = os.environ.get("PATH").split(":")
             for i in paths:
                 filename = f"{i}/{command}"
                 if os.path.isfile(filename) and os.access(filename, os.X_OK):
-                    subprocess.run(user_input.split())
-        else:
-            sys.stdout.write(f"{command}: command not found\n")
+                    subprocess.run(user_input)
+                else:
+                    sys.stdout.write(f"{command}: command not found\n")
 
 
 
