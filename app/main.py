6b29462199633f1f8643b0e9b27d421143d4d80f
Sarah Robinson <sarahrose@Sarahs-MacBook-Pro-8.local>
2025-11-30 01:42:32 +0000

    debugging cd

--- app/main.py
+++ app/main.py
@@ -28,10 +28,10 @@ def pwd_command(args):
 def cd_command(args):
     dir_path = args
     print(dir_path)
-    if os.path.exists(dir_path):
-        os.chdir(dir_path)
-    else:
-        print(f"cd: {dir_path}: No such file or directory")
+    # if os.path.exists(dir_path):
+    #     os.chdir(dir_path)
+    # else:
+    #     print(f"cd: {dir_path}: No such file or directory")
 
 commands = {
     "exit" : exit_command,
