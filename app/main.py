6d814cfb18ed817b8c1359a6ae793b92716be01d
Sarah Robinson <sarahrose@Sarahs-MacBook-Pro-8.local>
2026-01-28 17:13:32 +0000

    adds logic for second tab

--- app/main.py
+++ app/main.py
@@ -142,7 +142,7 @@ def shell_completer(text, state):
                 print("\x07")
             elif tab_count == 2 and not printed_list:
                 printed_list = True
-                print("  ".join(cached_matches))
+                print(" ".join(cached_matches))
                 print(f"$ {text}")
 
         last_prefix = text
